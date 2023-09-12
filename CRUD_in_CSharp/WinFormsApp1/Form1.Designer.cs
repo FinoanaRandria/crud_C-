32c00d702a311453460c58dd54f7a8ba7561b180
Finoana Randri <110087713+FinoanaRandria@users.noreply.github.com>
2023-09-12 11:49:15 +0200

    interface

--- CRUD_in_CSharp/WinFormsApp1/Form1.Designer.cs
+++ CRUD_in_CSharp/WinFormsApp1/Form1.Designer.cs
@@ -28,7 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            comboBox1 = new ComboBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(484, 62);
+            label1.Name = "label1";
+            label1.Size = new Size(82, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Crud en C#";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(110, 159);
+            label2.Name = "label2";
+            label2.Size = new Size(77, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Product id";
+            label2.Click += label2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(207, 159);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(294, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(110, 227);
+            label3.Name = "label3";
+            label3.Size = new Size(89, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Items Name";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(110, 286);
+            label4.Name = "label4";
+            label4.Size = new Size(55, 20);
+            label4.TabIndex = 4;
+            label4.Text = "Design";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(110, 343);
+            label5.Name = "label5";
+            label5.Size = new Size(45, 20);
+            label5.TabIndex = 5;
+            label5.Text = "Color";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(207, 224);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(294, 27);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(207, 279);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(294, 27);
+            textBox3.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(207, 335);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(294, 28);
+            comboBox1.TabIndex = 8;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.GhostWhite;
+            button1.BackgroundImageLayout = ImageLayout.Center;
+            button1.Enabled = false;
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(207, 398);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 9;
+            button1.Text = "insert";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.GhostWhite;
+            button2.BackgroundImageLayout = ImageLayout.Center;
+            button2.Enabled = false;
+            button2.ForeColor = Color.Black;
+            button2.Location = new Point(307, 398);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 10;
+            button2.Text = "update";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.GhostWhite;
+            button3.BackgroundImageLayout = ImageLayout.Center;
+            button3.Enabled = false;
+            button3.ForeColor = Color.Black;
+            button3.Location = new Point(407, 398);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 11;
+            button3.Text = "delete";
+            button3.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
@@ -36,11 +164,37 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.DarkOrchid;
             ClientSize = new Size(1118, 639);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private ComboBox comboBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
--- CRUD_in_CSharp/WinFormsApp1/Form1.cs
+++ CRUD_in_CSharp/WinFormsApp1/Form1.cs
@@ -6,5 +6,20 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
